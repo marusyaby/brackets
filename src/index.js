afe3c9e2615db35ef64e8d24a6479490c02853b0
marusyaby <mariakhrol@gmail.com>
2019-10-10 03:28:18 +0300

    feat: add brackets validation function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    var isSplittable = true;
+
+    while (isSplittable) {
+        isSplittable = false;
+
+        for (var i = 0; i < bracketsConfig.length; i++) {
+            var strCut = str.split(bracketsConfig[i][0] + bracketsConfig[i][1]);
+
+            if (strCut.length > 1) {
+                isSplittable = true;
+                str = strCut.join('');
+            }
+        }
+    }
+    return !str;
+};
+
+
+
+
+
+
